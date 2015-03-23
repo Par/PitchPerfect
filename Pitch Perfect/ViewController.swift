3e77e95d155878442ee439bc00301a3cc57e73f9
Parveen Yadav <Parveen@parveen-yadavs-macbook-pro.local>
2015-03-22 20:34:22 -0400

    added changes for 3a class

--- Pitch Perfect/ViewController.swift
+++ Pitch Perfect/ViewController.swift
@@ -12,6 +12,10 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var recordingInProgress: UILabel!
     
+    @IBOutlet weak var stopButton: UIButton!
+    
+    @IBOutlet weak var recordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,13 +25,22 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        stopButton.hidden = true
+        recordButton.enabled = true
+    }
 
     @IBAction func recordAudio(sender: UIButton) {
+        recordButton.enabled = false
+        stopButton.hidden = false
+
         recordingInProgress.hidden = false
         println("in recordAudio")
     }
 
     @IBAction func stopRecording(sender: UIButton) {
+
         recordingInProgress.hidden = true
 
     }
