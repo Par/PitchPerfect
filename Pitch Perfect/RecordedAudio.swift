1f351180de4ef7da03252097019399dfdf278e1b
Parveen Yadav <Parveen@parveen-yadavs-macbook-pro.local>
2015-03-22 23:40:10 -0400

    4a class work

---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Parveen Yadav on 3/22/15.
+//  Copyright (c) 2015 Parveen Yadav. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
